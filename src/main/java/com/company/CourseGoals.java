50804e40cfc74ee618c81d5c84e12eae84079dbc
Jacob Nardone <xjtn18@gmail.com>
2022-01-26 01:48:01 -0800

    goals fix

--- src/main/java/com/company/CourseGoals.java
+++ src/main/java/com/company/CourseGoals.java
@@ -1,2 +1,14 @@
-package com.company;public class CourseGoals {
+package com.company;
+
+public class CourseGoals {
+    /**
+     * Prints my bootcamp goals to the shell.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("My course goals:");
+        System.out.println("1. Expand my knowledge of Java and add practical back-end projects to my resume.");
+        System.out.println("2. Take advantage of the career services/resources to guide my entrance into the CS industry.");
+        System.out.println("3. Build lasting relationships with my mentor and the other students in the cohort.");
+    }
 }
